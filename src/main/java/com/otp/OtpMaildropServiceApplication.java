8cbd9ea10aae505e57ad492face18313701824b7
Solar Pablo <psolar@live.cl>
2026-03-30 00:21:13 -0300

    Create OtpMaildropServiceApplication.java file for Spring Boot application

---
+++ src/main/java/com/otp/OtpMaildropServiceApplication.java
@@ -0,0 +1,12 @@
+package com.otp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtpMaildropServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtpMaildropServiceApplication.class, args);
+    }
+}
